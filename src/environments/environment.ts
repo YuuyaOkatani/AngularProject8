6f590fc2ee89c2cbbdbc681e79031a8985844d04
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-03-28 13:05:31 -0300

    só falta colocar mais alguams coisas do supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
 
-    production: false,
-    supabaseUrl: 'https://maulmuzcyvfopbexgvxw.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdWxtdXpjeXZmb3BiZXhndnh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2MDA4NjYsImV4cCI6MjAyNzE3Njg2Nn0.IL5Ul8IHJYxPfyBepOi7lo3kdZSY5mqh9QPAvMXeV9I'
+    supabaseUrl: "https://maulmuzcyvfopbexgvxw.supabase.co",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdWxtdXpjeXZmb3BiZXhndnh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2MDA4NjYsImV4cCI6MjAyNzE3Njg2Nn0.IL5Ul8IHJYxPfyBepOi7lo3kdZSY5mqh9QPAvMXeV9I",
 
 };
